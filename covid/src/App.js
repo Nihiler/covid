af4dd8a86048006f13e1772391c37cfd23e845a6
jpr <julioperezrdlf@gmail.com>
2022-01-27 20:06:46 +0100

    i give up

--- covid/src/App.js
+++ covid/src/App.js
@@ -91,9 +91,9 @@ useEffect(() => {
           <CardContent>
             <h3>Total cases per country</h3>
             <Table countries={tableData}/>
-            
+           
             <h3>New cases (Worldwide)</h3>
-            <LineGraph/>
+            {/* <LineGraph/> */}
           </CardContent>
         </Card>
       
